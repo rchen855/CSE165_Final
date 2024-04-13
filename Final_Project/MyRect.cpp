8fa7d5e25295d395686bc31f0ca43bed791739a2
Ronald <ronald.c.855@gmail.com>
2024-04-12 18:56:54 -0700

    added movement to the object

--- Final_Project/MyRect.cpp
+++ Final_Project/MyRect.cpp
@@ -1,5 +1,17 @@
 #include "MyRect.h"
-#include <QDebug>
+#include <QKeyEvent>
+
 void MyRect::keyPressEvent(QKeyEvent *event) {
-    qDebug() << "MyRect knows that you pressed a key";
+    if (event->key() == Qt::Key_Left || event->key() == Qt::Key_A) {
+        setPos(x()-10,y());
+    }
+    else if (event->key() == Qt::Key_Right || event->key() == Qt::Key_D) {
+        setPos(x()+10,y());
+    }
+    else if (event->key() == Qt::Key_Up || event->key() == Qt::Key_W) {
+        setPos(x(),y()-10);
+    }
+    else if (event->key() == Qt::Key_Down || event->key() == Qt::Key_S) {
+        setPos(x(),y()+10);
+    }
 };
