c25292e8d49814a861fb20e5f163ca985b6a6520
Ronald <ronald.c.855@gmail.com>
2024-04-26 01:18:52 -0700

    removed unnecessary files

--- Final_Game/RegularEnemy.cpp
+++
@@ -1,7 +0,0 @@
-FasterEnemy::FasterEnemy(): Enemy() {
-    speed = 7; // Adjust speed for FasterEnemy
-    setPixmap(QPixmap(":/images/external/fasterenemy.png"));
-    QTimer* timer = new QTimer();
-    connect(timer, SIGNAL(timeout()), this, SLOT(move()));
-    timer->start(50);
-}
